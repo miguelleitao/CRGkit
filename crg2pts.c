4b9c76f0b38c95433e4eb961bcdb7652e8518967
Miguel Leitão <jml@isep.ipp.pt>
2019-12-03 01:28:13 +0000

    auto update

--- crg2pts.c
+++ crg2pts.c
@@ -31,13 +31,13 @@
 void usage()
 {
     crgMsgPrint( dCrgMsgLevelNotice, "usage: cgr2pts [options] <filename>\n" );
-    crgMsgPrint( dCrgMsgLevelNotice, "       options: -h            Show this info\n" );
+    crgMsgPrint( dCrgMsgLevelNotice, "       options: -h            Show this info.\n" );
     crgMsgPrint( dCrgMsgLevelNotice, "       options: -q            Produce PTSQ file using position and quaternion per vertex.\n");
     crgMsgPrint( dCrgMsgLevelNotice, "                              Format: [x,y,z,q1,q2,q3,q4]\n" );
     crgMsgPrint( dCrgMsgLevelNotice, "       options: -p            Produce Path file using U offset as indexing value in first column.\n" );
     crgMsgPrint( dCrgMsgLevelNotice, "                              U offset can be used as time when speed=1.\n");
-    crgMsgPrint( dCrgMsgLevelNotice, "       options: -v offset     Apply transverse offset\n" );
-    crgMsgPrint( dCrgMsgLevelNotice, "       <filename>             Identify input file\n" );
+    crgMsgPrint( dCrgMsgLevelNotice, "       options: -v offset     Apply transverse offset.\n" );
+    crgMsgPrint( dCrgMsgLevelNotice, "       <filename>             Identify input file.\n" );
     exit( -1 );
 }
 
@@ -45,8 +45,7 @@ int main( int argc, char** argv )
 {
     char*  filename = "";
     int    dataSetId = 0;
-    int    i;
-    int    j;
+    int    i, j;
     int    nStepsV = 20;
     int    cpId;
     int    formatPTSQ = 0;
@@ -134,62 +133,63 @@ int main( int argc, char** argv )
     crgDataSetGetURange( dataSetId, &uMin, &uMax );
     crgDataSetGetVRange( dataSetId, &vMin, &vMax );
     
-    
-    
     /* --- now test the position conversion --- */
     //du = ( uMax - uMin ) / nStepsU;
     du = 1.; 
     dv = ( vMax - vMin ) / nStepsV;
     
     u = uMin;
-    
-    // double phiStart = 
-     
-    //for ( i = -nBorderU; i <= nStepsU+nBorderU; i++ )
-    
+    /*
+    double phiStart = 0.;
+    CrgDataStruct *crgData = crgDataSetAccess( dataSetId );
+    if ( crgData ) phiStart = crgData->channelPhi.info.first
+    */
     double lastX=0, lastY=0;
     for( i=0 ; u<uMax ; i++ )
     {
         u = uMin + du * i;
         j = 0;
         
-            v = vMin + dv * j;
+        v = vMin + dv * j;
+        
+        if ( !crgEvaluv2xy( cpId, u, v, &x, &y ) ) {
+            crgMsgPrint( dCrgMsgLevelWarn, "main: error converting u/v = %.4f / %.4f to x/y.\n", u, v );
+            continue;
+        }
+        if ( !crgEvaluv2z( cpId, u, v, &z ) ) {
+            crgMsgPrint( dCrgMsgLevelWarn, "main: error converting u/v = %.4f / %.4f to z.\n", u, v );
+            continue;
+        }
+        if ( !crgEvaluv2pk( cpId, u, v, &phi, &curv ) ) {
+            crgMsgPrint( dCrgMsgLevelWarn, "main: error converting u/v = %.4f / %.4f to phi/kappa.\n", u, v );  
+            continue;
+        }
+  /*      
+        if ( i>0 ) 
+            phi = atan2( y-lastY, x-lastX );
+        else
+            phi = phiStart;
+    */        
+        if ( formatPath ) {
+            printf("%+10.4f ", du*i);
+        }
+        // Point position: X, Y, Z  
+        printf("%+10.4f %+10.4f %+10.4f ", x, y+2., z+2.);
+        
+        if ( formatPTSQ ) {
+            quat q, qi;
+             quat_make_from_euler(q, M_PI/2.5, 0., phi-M_PI/2.);
+            //quat_set(q, 0.588166750948254, 0.378888908573427, 0.384865056926993, 0.601981690747812);
+            //quat_conjugated(q, qi);
+            printf("%+10.4f %+10.4f %+10.4f %+10.4f\n", q[1], q[2], q[3], q[0] );
+                //q[0], q[1], q[2], q[3]);
+        }
+        else
+            printf(" %+10.4f %+10.4f %+10.4f\n", phi, 0., 0.);
             
-            if ( !crgEvaluv2xy( cpId, u, v, &x, &y ) ) {
-                crgMsgPrint( dCrgMsgLevelWarn, "main: error converting u/v = %.4f / %.4f to x/y.\n", u, v );
-                continue;
-            }
-            if ( !crgEvaluv2z( cpId, u, v, &z ) ) {
-                crgMsgPrint( dCrgMsgLevelWarn, "main: error converting u/v = %.4f / %.4f to z.\n", u, v );
-                continue;
-            }
-            if ( !crgEvaluv2pk( cpId, u, v, &phi, &curv ) ) {
-                crgMsgPrint( dCrgMsgLevelWarn, "main: error converting u/v = %.4f / %.4f to phi/kappa.\n", u, v );  
-                continue;
-            }
-            
-            if ( i>0 ) 
-                phi = atan2( y-lastY, x-lastX );
-            
-            if ( formatPath ) {
-                printf("%+10.4f ", du*i);
-            }
-            // Point position: X, Y, Z  
-            printf("%+10.4f %+10.4f %+10.4f ", x, y, z+2.);
-            
-            if ( formatPTSQ ) {
-                quat q;
-                quat_make_from_euler(q, phi, 0., 0.);
-                printf("%+10.4f %+10.4f %+10.4f %+10.4f\n",
-                    q[0], q[1], q[2], q[3]);
-            }
-            else
-                printf(" %+10.4f %+10.4f %+10.4f\n", phi, 0., 0.);
-             
-            lastX = x;
-            lastY = y;        
+        lastX = x;
+        lastY = y;        
     }
-    
     crgDataSetRelease(dataSetId);
     crgMsgPrint( dCrgMsgLevelNotice, "main: done.\n" );
     
